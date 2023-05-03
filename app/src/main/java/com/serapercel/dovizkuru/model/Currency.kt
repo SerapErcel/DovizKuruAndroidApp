a3c2d8f60c8a88a2365ccbdf5c4d1cc44deead93
Serap <ercelserap@gmail.com>
2023-05-03 14:25:07 +0300

    Add model

---
+++ app/src/main/java/com/serapercel/dovizkuru/model/Currency.kt
@@ -0,0 +1,10 @@
+package com.serapercel.dovizkuru.model
+
+data class Currency(
+    val Isim: String,
+    val CurrencyName: String,
+    val ForexBuying: String,
+    val ForexSelling: String,
+    val BanknoteBuying: String,
+    val BanknoteSelling: String
+    )
