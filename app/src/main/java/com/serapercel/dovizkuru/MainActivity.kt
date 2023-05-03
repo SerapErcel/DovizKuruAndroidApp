b634ea635fa791c382f4fb6ebccff06d318b4d88
Serap <ercelserap@gmail.com>
2023-05-03 16:25:04 +0300

    Add spinner

--- app/src/main/java/com/serapercel/dovizkuru/MainActivity.kt
+++ app/src/main/java/com/serapercel/dovizkuru/MainActivity.kt
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
+import com.serapercel.dovizkuru.model.Currency
 
 class MainActivity : AppCompatActivity() {
     lateinit var tvDate: TextView
+    lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +24,29 @@ class MainActivity : AppCompatActivity() {
 
         val xml = XMLResult()
         val date = xml.xmlTarih()
+        val currencyList = xml.xmlDoviz()
 
         tvDate = findViewById<TextView?>(R.id.tvDate)
+        spinner = findViewById(R.id.spnCurrency)
         tvDate.text = "Bugün: $date"
+
+        val adapter : ArrayAdapter<Currency> = ArrayAdapter(this, android.R.layout.simple_spinner_item, currencyList)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
     }
 }
